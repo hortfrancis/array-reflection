b1ff7c37d9f93425dddc61774496905996e2aa0e
Alex Hort-Francis <ajhf@hotmail.co.uk>
2023-10-03 14:45:48 +0100

    Add a button that loads another image
    - Add a button to index.html and bind the fetchImage() function in a click event listener

--- js/imageFetcher.js
+++ js/imageFetcher.js
@@ -1,6 +1,5 @@
 const displayImageContainer = document.querySelector('.image-displayer__image-container');
 
-
 function fetchImage() {
     fetch('https://picsum.photos/200/300')
         .then(response => {
@@ -10,4 +9,7 @@ function fetchImage() {
         .catch(error => console.log(error));
 }
 
+document.querySelector('.image-displayer__another-image-button').addEventListener('click', fetchImage);
+
+// Display the first image on page load
 fetchImage();
