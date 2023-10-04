ae1728f42e57e1e95721f1436dab789688ff89de
Alex Hort-Francis <ajhf@hotmail.co.uk>
2023-10-04 17:08:24 +0100

    Refactor to use Unsplash API
    - Random images are now fetched from the Unsplash API, instead of the Lorem Picsum API

--- js/imageFetcher.js
+++ js/imageFetcher.js
@@ -1,14 +1,29 @@
 const fetchedImageContainer = document.querySelector('.fetched-image-displayer__image-container');
 let currentImageURL = '';
 
+// Because this is a front-end project, we need to call the API from the client browser.
+// This 'access key' only permits requests for image URLs. 
+const accessKey = 'zHaO4h1oOz3bR_zLHY2XX0TAKF7Jy4Ny0gimZmSPrx0';
+const randomImageEndpoint = 'https://api.unsplash.com/photos/random';
+
 function fetchImage() {
-    fetch('https://picsum.photos/200/300')
+    // Using Unsplash API
+    // https://unsplash.com/documentation#get-a-random-photo
+    fetch(`${randomImageEndpoint}?client_id=${accessKey}`)
         .then(response => {
-            console.log(response);
-            fetchedImageContainer.innerHTML = `<img src="${response.url}" alt="random image">`;
-            currentImageURL = response.url;
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json(); // (Returns a promise)
         })
-        .catch(error => console.log(error));
+        .then(data => {
+            console.log(data);
+            // Saving the URL of the 'small' version of the image
+            fetchedImageContainer.innerHTML = `<img src="${data.urls.small}" alt="random image">`;
+            currentImageURL = data.urls.small;
+        })
+        .catch(error => console.error(error));
 }
 
 // Fetch another image each time the button is pressed
